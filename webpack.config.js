18c0a5598f4430270336a1e14f4c3b15da474723
kris10ansn <kristian.nessa@protonmail.com>
2025-09-12 13:20:30 +0200

    update webpack config, index.js changes to index-bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require("path");
 
-module.exports = {
-    mode: "production",
-    entry: "./src/index.ts",
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: "css-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-                include: [path.resolve(__dirname, "src")],
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts"],
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.js",
-    },
+module.exports = function setupWebpack(_env, argv) {
+    const production = argv.mode === "production";
+
+    const config = {
+        mode: production ? "production" : "development",
+        entry: {
+            index: "./src/index.ts",
+        },
+        output: {
+            path: path.resolve(__dirname, "./public"),
+            filename: "[name]-bundle.js",
+        },
+        resolve: {
+            extensions: [".ts"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                },
+            ],
+        },
+    };
+
+    if (!production) {
+        Object.assign(config, { devtool: "inline-source-map" });
+    }
+
+    return config;
 };
